04d9e4ce11eb271ae9e54ba6c0df1a62534d01c9
KravetsBohdan <bodyakravets@gmail.com>
2022-11-14 10:56:50 +0200

    Add bubble sort to ArrayUtils

--- src/main/java/com/bkravets/utils/ArrayUtils.java
+++ src/main/java/com/bkravets/utils/ArrayUtils.java
@@ -23,4 +23,25 @@ public class ArrayUtils {
 
         return -1;
     }
+
+
+    public static void bubbleSort(int[] array) {
+
+        if(array == null) throw new IllegalArgumentException("Array could not be null");
+
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if(array[j] > array[j + 1]) {
+                    swap(array, j, j + 1);
+                }
+            }
+        }
+
+    }
+
+    private static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
 }
