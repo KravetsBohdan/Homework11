cb8cb9fb9bb6ba063b381d818c4a00cabcd2edb6
KravetsBohdan <bodyakravets@gmail.com>
2022-11-14 09:25:28 +0200

    Add binary search to ArrayUtils

---
+++ src/main/java/com/bkravets/utils/ArrayUtils.java
@@ -0,0 +1,26 @@
+package com.bkravets.utils;
+
+public class ArrayUtils {
+
+    public static int binarySearch(int[] array, int element) {
+
+        if(array == null) throw new IllegalArgumentException("Array could not be null");
+
+        int startIndex = 0;
+        int endIndex = array.length - 1;
+
+        while (startIndex < endIndex) {
+            int midIndex = (startIndex + endIndex) / 2;
+
+            if(element < array[midIndex]) {
+                endIndex = midIndex;
+            } else if(element > array[midIndex]) {
+                startIndex = midIndex;
+            } else {
+                return midIndex;
+            }
+        }
+
+        return -1;
+    }
+}
